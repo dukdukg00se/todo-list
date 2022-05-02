44aeb24a244dc0883ec07d5f0691dc4829064469
Will H <will.wkhu@gmail.com>
2022-05-02 15:33:22 -0700

    Work on function to add projects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('Hello world');
+let allProjects = [];
+
+// const projectFactory = projName => {
+//   const projName = {};
+//   return projName;
+// }
+
+
+
+
+
+const projectForm = document.querySelector('.project-form');
+let projectTitle = document.getElementById('project-name');
+
+
+let btns = document.querySelectorAll('button');
+btns.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    console.log(e.target.id);
+
+    if (e.target.id === 'add-project') {
+      projectForm.classList.remove('hidden');
+    } 
+
+    if (e.target.id === 'cancel') {
+      projectForm.classList.add('hidden');
+    }
+
+    if (e.target.id === 'submit-project') {
+      console.log(projectTitle.value);
+    }
+
+  })
+})
