53bba3587103c7aaf7501e3a1fa1a93e24c0ef5b
Will H <will.wkhu@gmail.com>
2022-06-02 23:10:24 -0700

    Add methods to Project and Task prototype

--- src/index.js
+++ src/index.js
@@ -9,31 +9,84 @@ function Project(name) {
   this.tasks = [];
 }
 
+Project.prototype = {
+  constructor: Project,
+
+  addItem: function (container) {
+    container.push(this);
+  },
+
+  removeItem: function (itemId, container) {
+    for (let i = 0; i < container.length; i++) {
+      if (container[i].id === itemId) {
+        container.splice(i, 1);
+      }
+    }
+  },
+
+  setItemId: function (item, container) {
+    for (let i = 0; i < container.length; i++) {
+      item === 'project'
+        ? (container[i].id = 'project-' + i)
+        : (container[i].id = 'task-' + i);
+    }
+  }
+}
+
 function Task(name, details, date) {
   this.name = name;
   this.details = details;
   this.date = date;
 }
 
-Project.prototype = {
-  constructor: Project,
+Task.prototype = Object.create(Project.prototype);
 
-  addProject: function (container) {
-    container.push(this);
-  },
-  addTask: function (name, details, date) {
-    this.tasks.push(new Task(name, details, date));
-  },
-  removeTask: function (taskId) {
-    for (let i = 0; i < this.tasks.length; i++) {
-      if (this.tasks[i].id === taskId) {
-        this.tasks.splice(i, 1);
+
+
+
+const addListeners = () => {
+  const applicationButtons = document.querySelectorAll('button');
+  applicationButtons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      if (e.target.classList.contains('project')) {
+        const projectForm = document.querySelector("#projects-panel form");
+        const projectTextInput = document.querySelector("#project-name-input");
+        const addProjectButton = document.querySelector("#add-project");
+        const projectsPanel = document.querySelector('#projects-panel');
+
+        const showForm = () => {
+          projectForm.classList.toggle("hidden");
+          projectTextInput.focus();
+          addProjectButton.classList.toggle("hidden");
+        };
+      
+        const hideForm = () => {
+          projectForm.reset();
+          projectForm.classList.toggle("hidden");
+          addProjectButton.classList.toggle("hidden");
+        };
+
+        if (e.target.id === 'add-project') {
+          showForm();
+        }
+
+        if (e.target.id === 'cancel-project') {
+          hideForm();
+        }
+
+        if (e.target.id === 'submit-project') {
+          let projectName = projectTextInput.value;
+          projectName = new Project(projectName);
+          projectName.addItem(projectContainer);
+          projectName.setItemId('project', projectContainer);
+
+          console.log(projectContainer);
+
+        }
+
       }
-    }
-  },
-  setTaskId: function () {
-    for (let i = 0; i < this.tasks.length; i++) {
-      this.tasks[i].id = 'task-' + i;
-    }
-  }
+    })
+  })
 }
+
+addListeners();
